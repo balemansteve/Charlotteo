324b662a06df71422492285b599f35f1e7d9a8e4
balemansteve <balemansteve@gmail.com>
2025-06-11 17:00:48 -0300

    Add changes

--- poc_vmware_assistance/backend/app/api/v1/endpoints/prompt.py
+++ poc_vmware_assistance/backend/app/api/v1/endpoints/prompt.py
@@ -1,17 +1,38 @@
 """
 Modulo que maneja el endpoint POST /prompt.
 Punto de entrada general a la FastAPI.
-Envía mensaje del usuario a Bedrock.
+Envía mensaje del usuario a OpenAI.
 """
 
 from fastapi import APIRouter
 from app.schemas.prompt import PromptRequest
-from app.services.prompt_builder import PromtBuilder
-from poc_vmware_assistance.backend.app.services.openai_client import BedrockClient
+from app.services.prompt_builder import PromptBuilder
+from app.services.openai_client import OpenAIClient
 from app.services.vmware_client import VmwareClient
 
 router = APIRouter()
 
 prompt_builder = PromptBuilder()
-bedrock_client = BedrockClient()
+llm_client = OpenAIClient()
 vmware_client = VmwareClient()
+
+@router.post("/")
+def handle_prompt(request: PromptRequest):
+    # Contruir el prompt
+    prompt = prompt_builder.build_prompt(request.message)
+
+    # Enviar a OpenAI
+    response = llm_client.send_prompt(prompt)
+
+    # Si la respuesta es diccionario, hay una acción a ejecutar
+    if isinstance(response, dict):
+        script = response.get("script") # Script a ejecutar cuando se conecte a Aria
+        action = response.get("action")
+        vm_name = response.get("vm_name")
+
+        vmware_result = vmware_client.execute_script(vm_name, action)
+        final_response = llm_client.summarize_response(vmware_result)
+        return {"response": final_response}
+    
+    # Si la respuesta es texto plano, devolver directo
+    return {"response": response}
--- poc_vmware_assistance/backend/run.py
+++ poc_vmware_assistance/backend/run.py
@@ -4,11 +4,12 @@ Crea la instancia de FastAPI y se incluye el router.
 """
 
 from fastapi import FastAPI
-from app.api.v1.endpoints import prompt
+from app.api.v1.endpoints import prompt, metrics
 
 app = FastAPI(
-    title="VMware Assistance Poc"
+    title="VMware Assistance Poc",
     version="1.0"
 )
 
 app.include_router(prompt.router, prefix="/api/v1/prompt", tags=["Prompt"])
+app.include_router(metrics.router, prefif="/api/v1/metrics", tags=["Metrics"])
