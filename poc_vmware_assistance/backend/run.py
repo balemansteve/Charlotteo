87d6ca50c57eb8f3bd09764501d46ef56d0d2139
balemansteve <balemansteve@gmail.com>
2025-06-19 11:28:54 -0300

    Update CORS allowed origin to match frontend port

--- poc_vmware_assistance/backend/run.py
+++ poc_vmware_assistance/backend/run.py
@@ -15,11 +15,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 app.include_router(prompt.router, prefix="/api/v1/prompt", tags=["Prompt"])
-#  app.include_router(metrics.router, prefix="/api/v1/metrics", tags=["Metrics"])
+app.include_router(metrics.router, prefix="/api/v1/metrics", tags=["Metrics"])
