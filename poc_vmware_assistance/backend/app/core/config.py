2771e16b7474dfe672a6fabc55cfa4b412b98a81
balemansteve <balemansteve@gmail.com>
2025-06-30 16:21:56 -0300

    Add Aria API environments variables

--- poc_vmware_assistance/backend/app/core/config.py
+++ poc_vmware_assistance/backend/app/core/config.py
@@ -9,6 +9,9 @@ load_dotenv()
 
 class Config():
     OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+    ARIA_API_URL = os.getenv("ARIA_API_URL")
+    ARIA_API_USER = os.getenv("ARIA_API_USER")
+    ARIA_API_PASSWORD = os.getenv("ARIA_API_PASSWORD")
 
 
 settings = Config()
